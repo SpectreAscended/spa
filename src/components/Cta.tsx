5d526b2868b721c2a24a71b42188539457b395b7
Cory Pollard <astandard87@gmail.com>
2023-04-24 18:07:22 -0600

    Finished filling in markup, edited default styles for header elements

--- src/components/Cta.tsx
+++ src/components/Cta.tsx
@@ -7,6 +7,11 @@ const Cta: React.FC = () => {
       <p>
         Text or call <a href="tel:6395250611">639-525-0611</a>
       </p>
+      <h3>Open every day 10am to 9pm</h3>
+      <p>Appointment Only, No walk-ins please</p>
+      <h2>Location</h2>
+      <p>611, 3rd Ave. N.</p>
+      <p>Saskatoon, SK</p>
     </section>
   );
 };
