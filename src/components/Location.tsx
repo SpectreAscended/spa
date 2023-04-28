931288220a1582147f09924632a4db2fbffdf3d3
Cory Pollard <astandard87@gmail.com>
2023-04-27 18:56:59 -0600

    Crated location component

---
+++ src/components/Location.tsx
@@ -0,0 +1,12 @@
+import styles from './location.module.css';
+
+const Location: React.FC = () => {
+  return (
+    <section className={styles.location}>
+      <h2>Location</h2>
+      <figure></figure>
+    </section>
+  );
+};
+
+export default Location;
